c75d66880eff2a8c6fbfa1f7dc97837fc172d77e
NickStepanenko <massaalonso@yandex.ru>
2026-01-30 00:30:45 +0100

    Main area icons aligned

--- src/App.tsx
+++ src/App.tsx
@@ -47,30 +47,30 @@ function App() {
       </header>
       <main className='px-[130px]'>
         <section className='relative'>
-          <div className='flex relative py-40'>
+          <div className='flex relative py-39'>
             <div className='flex-1 relative'>
-              <img src={Pics} alt="Work with pics" className='absolute top-[10px]' />
-              <img src={Macpaw} alt="Macpaw App" className='absolute top-[200px]' />
-              <img src={PDF} alt="PDF App" className='absolute top-[300px]' />
-              <img src={Teamwork} alt="Teamwork App" className='absolute top-[400px]' />
-              <img src={Secure} alt="Secure App" className='absolute top-[500px]' />
+              <img src={Pics} alt="Work with pics" className='absolute top-[28px] -left-[14px]' />
+              <img src={Macpaw} alt="Macpaw App" className='absolute top-[159px] -left-[15px]' />
+              <img src={PDF} alt="PDF App" className='absolute top-[152px] left-[89px]' />
+              <img src={Teamwork} alt="Teamwork App" className='absolute top-[320px] -left-[19px]' />
+              <img src={Secure} alt="Secure App" className='absolute top-[400px] -left-[20px]' />
             </div>
             <div className='flex flex-col flex-2 margin-x-auto items-center text-center gap-14'>
-              <img src={Logo} alt="Logo" className='w-[128.75px] h-[128.75px] bg-black p-4 rounded-4xl' />
+              <img src={Logo} alt="Logo" className='w-[128px] h-[128px] bg-black p-4 rounded-4xl' />
               <h1 className='text-7xl/19 font-bold'>Dozens of apps. One subscription. $9.99</h1>
               <div className='flex gap-3'>
                 <button className='bg-white text-[#26262B] px-8 py-2 rounded-md font-medium hover:cursor-pointer mr-3'>Try free for 7 days</button>
                 <button className='bg-white text-[#26262B] px-4 py-2 rounded-md font-medium hover:cursor-pointer'><img src={Apple} alt="Apple Logo" /></button>
                 <button className='bg-white text-[#26262B] px-4 py-2 rounded-md font-medium hover:cursor-pointer'><img src={Google} alt="Google Logo" /></button>
               </div>
-              <p className='text-lg px-36'>Power up your workflow with Setapp, a smart way to get apps.</p>
+              <p className='text-lg/8 px-36'>Power up your workflow with Setapp, a smart way to get apps.</p>
             </div>
             <div className='flex-1 relative'>
-              <img src={Wifi} alt="Wifi App" className='absolute' />
-              <img src={Plan} alt="Plan App" className='absolute' />
-              <img src={Converter} alt="Converter App" className='absolute' />
-              <img src={Manage} alt="Manage App" className='absolute' />
-              <img src={Code} alt="Code App" className='absolute' />
+              <img src={Wifi} alt="Wifi App" className='absolute top-[46px] left-[153px]' />
+              <img src={Plan} alt="Plan App" className='absolute top-[106px] left-[62px]' />
+              <img src={Manage} alt="Manage App" className='absolute top-[318px] left-[81px]' />
+              <img src={Converter} alt="Converter App" className='absolute top-[217px] left-[214px]' />
+              <img src={Code} alt="Code App" className='absolute top-[422px] left-[140px]' />
             </div>
           </div>
         </section>
